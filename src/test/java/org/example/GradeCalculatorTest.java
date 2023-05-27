3237a59dd78cef205d33810eb0801f0a3dcdeb55
Nohbin <nohbinmusic@gmail.com>
2023-05-27 13:38:10 +0900

    Initial commit

---
+++ src/test/java/org/example/GradeCalculatorTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/*
+*   평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+    MVC패턴(Model-View-Controller) 기반으로 구현한다.
+    일급 컬렉션 사용
+* */
+public class GradeCalculatorTest {
+    //학점계산기 도메인 : 이수한 과목 , 학점 계산기
+
+
+}
